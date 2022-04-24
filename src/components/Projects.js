e86cbf86280d7010fc3231eb3a562191d7349e5f
Adrian B <adrian.biedny@gmail.com>
2022-04-24 14:59:57 +0200

    Working github projects fetch, still need to style

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { projects } from "../data";
 
-function createRepositories() {
-  return (
-    ""
-  );
+async function getProjects(e) {
+  e.preventDefault();
+  try 
+  {
+    const url = 'https://api.github.com/users/NotAffiche/repos';
+  const response = await fetch(url);
+  const json = await response.json();
+  console.log(json);
+  let result = "";
+
+  for (let i = 0; i < json.length; i++) {
+    result += json[i].name + " \n";
+  } 
+
+  document.getElementById('projects-content').innerText = result;
+  document.getElementById('getProjectsButton').style.display = 'none';
+  } catch (err) {
+    document.getElementById('projects-content').innerText = err;
+    return;
+  }
 }
 
 export default function Projects() {
@@ -19,8 +34,10 @@ export default function Projects() {
             Here are some of the projects that I have worked on (these are the public ones on my <a href="https://github.com/NotAffiche?tab=repositories" className="text-primairy">Github</a>).
           </p>
         </div>
-        <div className="flex flex-wrap m-4">
-          {createRepositories()}
+        <div className="">
+          <button id="getProjectsButton" className="rounded-full px-4 py-2 bg-primairy" onClick={getProjects}>Get Projects</button>
+          <div className="flex flex-wrap m-4 text-gray-200" id="projects-content">
+          </div>
         </div>
       </div>
     </section>
